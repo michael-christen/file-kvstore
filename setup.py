065fa529410f26a3e7653e12952cacfd7ee29751
karatekid <mchristen96@gmail.com>
2017-04-27 22:35:16 -0700

    Fix setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,10 @@ setup(
     author='Michael Christen',
     url='https://github.com/michael-christen/file-kvstore',
     license='MIT',
-    packages=find_packages(exclude=["tests*"]),
+    packages=find_packages(exclude=["*.tests"]),
+    install_requires=[
+        'PyYAML',
+    ],
     package_data={},
     data_files=[],
     entry_points={
